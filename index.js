1cc9979c13097109cd705d1ca45741ae0bce65c6
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-10-24 10:05:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello World'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen({port: 5000})
+.then(res=> {
+    console.log(`Run on ${res.url}`)
+})
